11a488e4ac9f8bfb54e733ca950468a1ebf2ea57
Kulis27 <fila.kulis@gmail.com>
2017-04-18 11:39:07 +0200

    Update hello.py

--- hello.py
+++ hello.py
@@ -3,3 +3,12 @@ import multiprocessing as mp
 
 print("Python version %s " % str(sys.version))
 print("Cores          %s " % str(mp.cpu_count()))
+
+
+if True:
+  print("tady")
+else:
+  print("tady nikdy")
+  a = 0
+  b = 1
+  c = b / a
